0d37cf56330bfe7e0e9035f751edfee577086ad2
4thefade <122586087+4thefade@users.noreply.github.com>
2023-01-29 18:55:04 +0000

    Pending changes exported from your codespace

--- sendtoken.js
+++ sendtoken.js
@@ -1,23 +1,23 @@
-//Transfer ERC20 token
+//Transfer ERC20 token, make sure to change node to mainnet and AmounttoSend needs to be exact amount of PLSx in hacked wallet to send
 
 const {ethers} = require('ethers')
 
-const node = 'Quicknode https://bit.ly/3QxPwWk'
+const node = 'https://rpc.v2b.testnet.pulsechain.com'
 const provider = new ethers.providers.JsonRpcProvider(node)
 
 
-const tokenAddress = '0x01BE23585060835E02B77ef475b0Cc51aA1e0709'  //Chainlink TokenAddress
+const tokenAddress = '0x07895912f3AB0E33aB3a4CEFbdf7a3e121eb9942'  //Chainlink TokenAddress
 const tokenAbi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transferAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"}]
 
 
-const privatekey = 'sender PrivateKey'
+const privatekey = '863a0bef9b0d81af6f6f64e6db8cfe06f90d5720a5dfa55cf21ced053e067337'
 const wallet = new ethers.Wallet(privatekey, provider)
 const contract = new ethers.Contract(tokenAddress, tokenAbi, wallet)
 
-const receiver= 'receiver'
-const sender = 'sender'
+const receiver= '0x718942a23D6EB653E7eD2fe194DA5E789793A193'
+const sender = '0x73c09513aCEE8b0BD17c171BA1b4f91168555C86'
 
-const amountToSend = '1'
+const amountToSend = '4031732'
 
 
 async function main(){
